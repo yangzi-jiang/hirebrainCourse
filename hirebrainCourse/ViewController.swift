bbe4126fa1d2f87191f00a3132aa82f53212e88b
Yangzi Jiang <yzj999@gmail.com>
2020-10-15 22:43:59 -0400

    Initial Commit

---
+++ hirebrainCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hirebrainCourse
+//
+//  Created by Yangzi Jiang on 10/15/20.
+//  Copyright © 2020 HireBrain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
